45908f868036d8e993b289bb439181323e787a60
Husbaan <33893372+ZOXT@users.noreply.github.com>
2026-08-19 01:20:06 +0500

    Add: majority-element - submission-0

---
+++ Data Structures & Algorithms/majority-element/submission-0.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+
+        unordered_map<int, int> count;
+        int result=0, maxCount =0;
+        for(int num : nums){
+            count[num]++;
+            if(count[num] > maxCount){
+                result = num;
+                maxCount = count[num];
+            }
+
+        }
+        return result;
+
+        
+    }
+};
